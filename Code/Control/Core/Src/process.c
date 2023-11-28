6c01f832e4531f56a3ea8c1807d931c58e4ad05f
Justin_Lau <justin610810@outlook.com>
2023-11-29 05:59:10 +0800

    Convert solution to movement

--- Code/Control/Core/Src/process.c
+++ Code/Control/Core/Src/process.c
@@ -71,188 +71,36 @@ void makeOneMovement(char Movement[]) {
 				movement_R();
 			break;
 		case 'L':
-			if (Movement[2] != '\0')
-				movement_aL2();
-			else if (Movement[1] == '\0' && Movement[2] == '\0')
-				movement_L();
-			else if (Movement[1] == '2')
+			if (Movement[1] == '2')
 				movement_L2();
+			else if (Movement[1] == '\'')
+				movement_aL();
 			else
 				movement_L();
 			break;
 		case 'B':
-			if (Movement[2] != '\0')
-				movement_aB2();
-			else if (Movement[1] == '\0' && Movement[2] == '\0')
-				movement_B();
-			else if (Movement[1] == '2')
+			if (Movement[1] == '2')
 				movement_B2();
+			else if (Movement[1] == '\'')
+				movement_aB();
 			else
 				movement_B();
 			break;
 		case 'U':
-			if (Movement[2] != '\0')
-				movement_aU2();
-			else if (Movement[1] == '\0' && Movement[2] == '\0')
-				movement_U();
-			else if (Movement[1] == '2')
+			if (Movement[1] == '2')
 				movement_U2();
+			else if (Movement[1] == '\'')
+				movement_aU();
 			else
 				movement_U();
 			break;
 		case 'D':
-			if (Movement[2] != '\0')
-				movement_aD2();
-			else if (Movement[1] == '\0' && Movement[2] == '\0')
-				movement_D();
-			else if (Movement[1] == '2')
+			if (Movement[1] == '2')
 				movement_D2();
+			else if (Movement[1] == '\'')
+				movement_aD();
 			else
 				movement_D();
-			break;
-		case 'X':
-			if (Movement[2] != '\0') {
-				movement_aX();
-				movement_aX();
-			} else if (Movement[1] == '\0' && Movement[2] == '\0')
-				movement_X();
-			else if (Movement[1] == '2') {
-				movement_X();
-				movement_X();
-			} else
-				movement_aX();
-			break;
-		case 'Y':
-			if (Movement[2] != '\0') {
-				movement_aY();
-				movement_aY();
-			} else if (Movement[1] == '\0' && Movement[2] == '\0')
-				movement_Y();
-			else if (Movement[1] == '2') {
-				movement_Y();
-				movement_Y();
-			} else
-				movement_aY();
-			break;
-		case 'Z':
-			if (Movement[2] != '\0') {
-				movement_aZ();
-				movement_aZ();
-			} else if (Movement[1] == '\0' && Movement[2] == '\0')
-				movement_Z();
-			else if (Movement[1] == '2') {
-				movement_Z();
-				movement_Z();
-			} else
-				movement_aZ();
-			break;
-		case 'M':
-			if (Movement[2] != '\0') {
-				movement_aM();
-				movement_aM();
-			} else if (Movement[1] == '\0' && Movement[2] == '\0')
-				movement_M();
-			else if (Movement[1] == '2') {
-				movement_M();
-				movement_M();
-			} else
-				movement_aM();
-			break;
-		case 'E':
-			if (Movement[2] != '\0') {
-				movement_aE();
-				movement_aE();
-			} else if (Movement[1] == '\0' && Movement[2] == '\0')
-				movement_E();
-			else if (Movement[1] == '2') {
-				movement_E();
-				movement_E();
-			} else
-				movement_aE();
-			break;
-		case 'S':
-			if (Movement[2] != '\0') {
-				movement_aS();
-				movement_aS();
-			} else if (Movement[1] == '\0' && Movement[2] == '\0')
-				movement_S();
-			else if (Movement[1] == '2') {
-				movement_S();
-				movement_S();
-			} else
-				movement_aS();
-			break;
-		case 'u':
-			if (Movement[2] != '\0') {
-				movement_au();
-				movement_au();
-			} else if (Movement[1] == '\0' && Movement[2] == '\0')
-				movement_u();
-			else if (Movement[1] == '2') {
-				movement_u();
-				movement_u();
-			} else
-				movement_au();
-			break;
-		case 'l':
-			if (Movement[2] != '\0') {
-				movement_al();
-				movement_al();
-			} else if (Movement[1] == '\0' && Movement[2] == '\0')
-				movement_l();
-			else if (Movement[1] == '2') {
-				movement_l();
-				movement_l();
-			} else
-				movement_al();
-			break;
-		case 'f':
-			if (Movement[2] != '\0') {
-				movement_af();
-				movement_af();
-			} else if (Movement[1] == '\0' && Movement[2] == '\0')
-				movement_f();
-			else if (Movement[1] == '2') {
-				movement_f();
-				movement_f();
-			} else
-				movement_af();
-			break;
-		case 'r':
-			if (Movement[2] != '\0') {
-				movement_ar();
-				movement_ar();
-			} else if (Movement[1] == '\0' && Movement[2] == '\0')
-				movement_r();
-			else if (Movement[1] == '2') {
-				movement_r();
-				movement_r();
-			} else
-				movement_ar();
-			break;
-		case 'b':
-			if (Movement[2] != '\0') {
-				movement_ab();
-				movement_ab();
-			} else if (Movement[1] == '\0' && Movement[2] == '\0')
-				movement_b();
-			else if (Movement[1] == '2') {
-				movement_b();
-				movement_b();
-			} else
-				movement_ab();
-			break;
-		case 'd':
-			if (Movement[2] != '\0') {
-				movement_ad();
-				movement_ad();
-			} else if (Movement[1] == '\0' && Movement[2] == '\0')
-				movement_d();
-			else if (Movement[1] == '2') {
-				movement_d();
-				movement_d();
-			} else
-				movement_ad();
 			break;
 	}
 }
